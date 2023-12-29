c207f91a2cc0b65f43bf2f36c58d5828f64cc208
swh_7 <swh_77@naver.com>
2023-12-30 00:04:20 +0900

    commitTest

--- src/main/java/com/hayeum/backserver/Application/te/controller/TestController.java
+++ src/main/java/com/hayeum/backserver/Application/te/controller/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
 	public HashMap<String, Object> test(@RequestBody HashMap<String, Object> resultMap){
 		log.info("파라미터 ? [{}]",resultMap.toString());
 		log.info("통신 성공");
-
+		log.info("[{}]","커밋테스트");
 		HashMap<String, Object> returnMap = new HashMap<>();
 		returnMap.put("result", "00000");
 		return returnMap;
