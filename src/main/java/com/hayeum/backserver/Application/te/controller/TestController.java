9bcaf03d6fab2bb02bca23b303d611d9192f3694
김규석 <kyu05017@naver.com>
2024-01-02 20:31:13 +0900

    JPA 진행중

--- src/main/java/com/hayeum/backserver/Application/te/controller/TestController.java
+++ src/main/java/com/hayeum/backserver/Application/te/controller/TestController.java
@@ -1,12 +1,11 @@
 package com.hayeum.backserver.Application.te.controller;
 
-import java.io.IOException;
 import java.util.HashMap;
-
-import com.hayeum.backserver.common.object.SendMap;
-import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
+import com.hayeum.backserver.common.object.SendMap;
 
 @RestController
 @RequestMapping("/TestController")
@@ -14,9 +13,22 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
 	@PostMapping("/test")
-	public HashMap<String, Object> test(HttpServletRequest request)throws IOException {
+	public HashMap<String, Object> test(HttpServletRequest request)throws Exception {
 		SendMap<String,Object> newSendMap = new SendMap<String,Object>(request);
 		log.info("통신 성공");
+		// 한줄 복사는 컨트롤 + D
+		// 줄이동은 알트 + 쉬프트 + 화살표
+		// 객체 자동생성은 V + 엔터
 		return new HashMap<>();
 	}
-}
+
+	@GetMapping(value = "/test2")
+	public HashMap<String, Object> test2(HttpServletRequest request) throws Exception {
+		SendMap<String,Object> newSendMap = new SendMap<String,Object>(request);
+		log.info("통신 성공");
+
+		log.info(newSendMap.toString());
+
+		return new HashMap<>();
+	}
+ }
