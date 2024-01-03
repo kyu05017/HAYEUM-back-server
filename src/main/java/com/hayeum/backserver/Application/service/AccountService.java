997ec45dfc51bda0661bfc8b248fe6e9a59905f5
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-01-04 01:31:35 +0900

    회원정보 저장 1차 수정 font-back

--- src/main/java/com/hayeum/backserver/Application/service/AccountService.java
+++ src/main/java/com/hayeum/backserver/Application/service/AccountService.java
@@ -3,41 +3,36 @@ package com.hayeum.backserver.Application.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hayeum.backserver.common.object.UserInfoDto;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service @Slf4j
 public class AccountService {
 
-    @Value("${testUser.token}")
-    private String token;
-    @Value("${testUser.name}")
-    private String name;
-    @Value("${testUser.postCnt}")
-    private int postCnt;
 
 
-    @Autowired
-    HttpSession session;
     private final static ObjectMapper objectMapper = new ObjectMapper();
-    public boolean findByToken()
+    public HashMap<String,Object> findByToken(String getToken)
     {
+        HashMap<String,Object> result = new HashMap<>();
         try{
-            if(session.getAttribute("token").equals(token))
+            String token = "1111";
+            if(getToken.equals(token))
             {
-                session.setAttribute("Info",objectMapper.writeValueAsString(new UserInfoDto(token,name,postCnt)));
                 log.info("토큰 정상 생성");
-                return true;
+                result.put("info",objectMapper.writeValueAsString(new UserInfoDto("kim","aaa@naver.com","1234")));
+            }else{
+                result.put("error","tokenValueError");
             }
-        }catch(NullPointerException e) {
-            log.info("token null ->{}",e );
         } catch (JsonProcessingException e) {
+            result.put("error","loginJsonError");
             throw new RuntimeException(e);
         }
-        return false;
+
+        return result;
     }
 
 }
--- src/main/java/com/hayeum/backserver/common/object/UserInfoDto.java
+++ src/main/java/com/hayeum/backserver/common/object/UserInfoDto.java
@@ -3,14 +3,15 @@ package com.hayeum.backserver.common.object;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-@AllArgsConstructor
+@Getter @Setter @AllArgsConstructor
 public class UserInfoDto {
 
     private String name;
 
     private String email;
+    /* 중요 데이터 ex) 패스워드 */
     @JsonIgnore
-    private int postCnt;
+    private String pwd;
 
 
 }
