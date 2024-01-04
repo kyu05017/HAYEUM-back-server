6702d112f93954d45b04a70903544f54dc0e1734
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-01-04 21:51:46 +0900

    요청 매소드 수정

--- src/main/java/com/hayeum/backserver/Application/te/controller/AccountController.java
+++ src/main/java/com/hayeum/backserver/Application/te/controller/AccountController.java
@@ -5,7 +5,7 @@ import com.hayeum.backserver.common.object.SendMap;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +20,14 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public HashMap<String, Object> doLogin(HttpServletRequest request)throws IOException {
         /* 통신 객체 생성*/
         SendMap<String,Object> newSendMap = new SendMap<String,Object>(request);
 
         return accountService.findByToken(newSendMap.getHeaderIn().getValue("token",""));
     }
-    @GetMapping("/my-info")
+    @PostMapping("/my-info")
     public HashMap<String,Object> myPage(HttpServletRequest request) throws  IOException{
         SendMap<String,Object> newSendMap = new SendMap<>(request);
         log.info("마이 페이지 입장");
